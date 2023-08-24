15c2a34906ecd6c397af5d86ffdb71c56b62914b
spiderthatcodes <jessicaldickerson@outlook.com>
2023-08-24 12:14:11 -0700

    full CRUD and html template added

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import show_recipes, recipe_list, create_recipe
+from .views import show_recipes, recipe_list, create_recipe, edit_recipe, delete_recipe
 
 urlpatterns = [
     path("recipes/", recipe_list, name='recipe_list'),
     path("recipes/<int:id>", show_recipes, name="show_recipes"),
-    path("recipes/add/", create_recipe, name="create_recipe")
+    path("recipes/add/", create_recipe, name="create_recipe"),
+    path("recipes/<int:id>/edit", edit_recipe, name="edit_recipe"),
+    path("recipes/<int:id>/delete", delete_recipe, name="delete_recipe")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,26 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Recipe
 from .forms import RecipeForm
 
 
-# this controller is for the details page
+
 def show_recipes(request, id):
     recipe = get_object_or_404(Recipe, id=id)
     context = {
         "recipe_object": recipe,
     }
 
-    return render(request, "detail.html", context)
+    return render(request, "recipes/detail.html", context)
 
-# list page controller
+
 def recipe_list(request):
     recipes = Recipe.objects.all()
     context = {
         "recipe_list": recipes,
     }
-    return render(request, "list.html", context)
+    return render(request, "recipes/list.html", context)
 
-# create recipe controller
+
 def create_recipe(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
@@ -32,4 +33,25 @@ def create_recipe(request):
     context = {
         'form': form,
     }
-    return render(request, 'create.html', context)
+    return render(request, 'recipes/create.html', context)
+
+
+def edit_recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('show_recipes', id)
+    else:
+        form = RecipeForm()
+        context = {
+            'form': form,
+            }
+        return render(request, 'recipes/edit.html', context)
+
+
+def delete_recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
+    recipe.delete()
+    return redirect('recipe_list')
